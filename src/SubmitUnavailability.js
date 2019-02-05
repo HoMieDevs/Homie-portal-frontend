b8325c08dae670190d4eb5299ca6941560a45487
cryptokitties0327 <kimberley_tse@yahoo.com.au>
2019-02-05 12:49:30 +1100

    with sorting functionality - unavailability

--- src/SubmitUnavailability.js
+++ src/SubmitUnavailability.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import "./css/Register.css";
 axios.defaults.withCredentials = true;
 
+var moment = require("moment");
+
 class SubmitUnavailability extends Component {
   state = {
     UserUnavailability: []
@@ -12,14 +14,30 @@ class SubmitUnavailability extends Component {
     const userId = localStorage.getItem("userId");
     axios
       .get(`http://localhost:5000/auth/unavailibility/${userId}`)
-      .then(resp => this.setState(resp.data));
+      .then(resp => {
+        console.log(resp.data.UserUnavailability);
+        const sorted = resp.data.UserUnavailability.sort((a, b) => {
+          return a.date > b.date ? -1 : a.date < b.date ? 1 : 0;
+          // return new Date(a.date) > new Date(b.date);
+        });
+        console.log(sorted);
+        this.setState({ UserUnavailability: sorted });
+      });
   }
 
   componentDidUpdate() {
     const userId = localStorage.getItem("userId");
     axios
       .get(`http://localhost:5000/auth/unavailibility/${userId}`)
-      .then(resp => this.setState(resp.data));
+      .then(resp => {
+        console.log(resp.data.UserUnavailability);
+        const sorted = resp.data.UserUnavailability.sort((a, b) => {
+          return a.date > b.date ? -1 : a.date < b.date ? 1 : 0;
+          // return new Date(a.date) > new Date(b.date);
+        });
+        console.log(sorted);
+        this.setState({ UserUnavailability: sorted });
+      });
   }
 
   render() {

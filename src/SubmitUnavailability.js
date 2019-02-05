f3939b434d7bbc029f2c6fec2702c4f46ab9b87c
timothydcole <tim@egeekmedia.com.au>
2019-02-05 12:47:11 +1100

    added delete button to time-off

--- src/SubmitUnavailability.js
+++ src/SubmitUnavailability.js
@@ -29,7 +29,7 @@ class SubmitUnavailability extends Component {
     .then(console.log('Deleted'))
     .catch(err => console.log(err))
   }
-
+  
   render() {
     return (
       <div className="main">

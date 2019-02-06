1fd3370a7e75f65564b4d967895adc7c5a669b67
cryptokitties0327 <kimberley_tse@yahoo.com.au>
2019-02-06 19:30:43 +1100

    deployed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,68 @@
-import React, { Component } from 'react';
-import Login from './Login'
-import Register from './Register'
-import Home from './Home'
-import Roster from './Roster'
-import RosterAddStaff from './RosterAddStaff'
-import RosterAdd from './RosterAdd'
-import RosterAdmin from './RosterAdmin'
-import { BrowserRouter, Route,  Redirect } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser, faHome, faUserTimes, faSignOutAlt, faCalendarAlt, faCalendarTimes,  faClock, faSync, faChevronUp, faUserPlus, faChevronCircleRight, faChevronCircleLeft} from '@fortawesome/free-solid-svg-icons'
-import RosterAll from './RosterAll';
-import RosterAllTest from './RosterAllTest';
-import ReactSelect from './ReactSelect';
-import TimeOff from './TimeOff';
-import SelectStaff from './SelectStaff';
-import MyInfo from './MyInfo';
-import ReportHours from './ReportHours';
-import AllStaff from './AllStaff';
+import React, { Component } from "react";
+import Login from "./Login";
+import Register from "./Register";
+import Home from "./Home";
+import Roster from "./Roster";
+import RosterAddStaff from "./RosterAddStaff";
+import RosterAdd from "./RosterAdd";
+import RosterAdmin from "./RosterAdmin";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUser,
+  faHome,
+  faUserTimes,
+  faSignOutAlt,
+  faCalendarAlt,
+  faCalendarTimes,
+  faClock,
+  faSync,
+  faChevronUp,
+  faUserPlus,
+  faChevronCircleRight,
+  faChevronCircleLeft
+} from "@fortawesome/free-solid-svg-icons";
+import RosterAll from "./RosterAll";
+import RosterAllTest from "./RosterAllTest";
+import ReactSelect from "./ReactSelect";
+import TimeOff from "./TimeOff";
+import SelectStaff from "./SelectStaff";
+import MyInfo from "./MyInfo";
+import ReportHours from "./ReportHours";
+import AllStaff from "./AllStaff";
 
-library.add(faUser, faHome, faUserTimes, faSignOutAlt, faCalendarAlt, faCalendarTimes, faClock, faSync, faChevronUp, faUserPlus, faChevronCircleRight, faChevronCircleLeft );
+library.add(
+  faUser,
+  faHome,
+  faUserTimes,
+  faSignOutAlt,
+  faCalendarAlt,
+  faCalendarTimes,
+  faClock,
+  faSync,
+  faChevronUp,
+  faUserPlus,
+  faChevronCircleRight,
+  faChevronCircleLeft
+);
 
-const PrivateRoute =  ({ component: Component, ...rest}) => (
-  <Route {...rest} render={(props) => (
-    localStorage.getItem("isAuthenticated")
-    ? <Component {...props} />
-    : <Redirect to={{
-      pathname: '/',
-      state: { from: props.location }
-    }} />
-  )} />
-)
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("isAuthenticated") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
 
 class App extends Component {
   render() {
@@ -37,24 +70,27 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <div className="App">
-          <Route exact path="/" component={Login} />
-          <PrivateRoute path="/register" component={Register} />
-          <PrivateRoute path="/home" component={Home} />
-          <PrivateRoute path="/timeoff" component={TimeOff} />
-          <PrivateRoute exact path="/rosteradmin" component={RosterAdmin} />
-          <PrivateRoute exact path="/roster" component={RosterAll} />
-          <PrivateRoute exact path="/testroster" component={RosterAllTest} />
-          <PrivateRoute path="/select" component={ReactSelect} />
-          <PrivateRoute path="/myinfo" component={MyInfo} />
-          <PrivateRoute path="/report" component={ReportHours} />
-          <PrivateRoute path="/roster/:id" component={Roster} />
-          <PrivateRoute path="/rosteraddstaff/:id" component={RosterAddStaff} />
-          <PrivateRoute exact path="/rosteradd" component={RosterAdd} />
-          <PrivateRoute path="/allstaff" component={AllStaff} />
-          <PrivateRoute path="/staffselect" component={SelectStaff} />
+            <Route exact path="/" component={Login} />
+            <PrivateRoute path="/register" component={Register} />
+            <PrivateRoute path="/home" component={Home} />
+            <PrivateRoute path="/timeoff" component={TimeOff} />
+            <PrivateRoute exact path="/rosteradmin" component={RosterAdmin} />
+            <PrivateRoute exact path="/roster" component={RosterAll} />
+            <PrivateRoute exact path="/testroster" component={RosterAllTest} />
+            <PrivateRoute path="/select" component={ReactSelect} />
+            <PrivateRoute path="/myinfo" component={MyInfo} />
+            <PrivateRoute path="/report" component={ReportHours} />
+            <PrivateRoute path="/roster/:id" component={Roster} />
+            <PrivateRoute
+              path="/rosteraddstaff/:id"
+              component={RosterAddStaff}
+            />
+            <PrivateRoute exact path="/rosteradd" component={RosterAdd} />
+            <PrivateRoute path="/allstaff" component={AllStaff} />
+            <PrivateRoute path="/staffselect" component={SelectStaff} />
           </div>
         </BrowserRouter>
-        </div>
+      </div>
     );
   }
 }
